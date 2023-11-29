f66f026db7c9150f37fb60f982c0709310b0962d
ashirbad121 <ashirbad.sonu123@gmail.com>
2023-11-29 13:37:55 +0530

    ok

---
+++ src/test/java/com/banking/TestScript1Test.java
@@ -0,0 +1,18 @@
+package com.banking;
+
+import org.testng.annotations.Test;
+
+public class TestScript1Test {
+
+	@Test(groups = "smoke")
+	public void ts1Test()
+	{
+		System.out.println("---Test script-1----");
+	}
+	
+	@Test(groups="regression")
+	public void ts2Test()
+	{
+		System.out.println("---Test script-2----");
+	}
+}
