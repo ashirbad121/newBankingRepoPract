fad45818fda75363fc00e8dbc0b31b680611760d
ashirbad121 <ashirbad.sonu123@gmail.com>
2023-11-30 12:29:07 +0530

    commit

--- src/test/java/com/banking/TestScript3Test.java
+++ src/test/java/com/banking/TestScript3Test.java
@@ -1,16 +1,44 @@
 package com.banking;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class TestScript3Test {
 
+	public WebDriver driver;
 	@Test(groups = "smoke")
 	public void ts1Test()
 	{
 		System.out.println("---Test script-1----");
-	}
+		String BROWSER = System.getProperty("browser");
+		String URL=System.getProperty("url");
+		
+		
+		if(BROWSER.equalsIgnoreCase("chrome"))
+		{
+			driver=new ChromeDriver();
+		}
+		else if(BROWSER.equalsIgnoreCase("firefox"))
+		{
+			driver=new FirefoxDriver();
+		}
+		else if(BROWSER.equalsIgnoreCase("edge"))
+		{
+			driver=new EdgeDriver();
+		}
+		else
+		{
+			System.out.println("invalid browser");
+		}
+		driver.manage().window().maximize();
+		driver.get(URL);
+			
+		}
 	
-	@Test(groups="regression")
+	/*@Test(groups="regression")
 	public void ts2Test()
 	{
 		System.out.println("---Test script-2----");
@@ -20,5 +48,5 @@ public class TestScript3Test {
 		System.out.println("hiiiii");
 		
 		System.out.println("hellooo");
-	}
+	}*/
 }
